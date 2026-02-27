bf462e4e8e548f274f3090878007dca0cdebc0da
Kshitz Yadav <ahirkshitiz.pb@gmail.com>
2026-02-27 11:53:36 +0530

    customui1

---
+++ app/src/main/java/com/example/newstart/CustomUI1.kt
@@ -0,0 +1,45 @@
+package com.example.newstart
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
+
+@Preview
+@Composable
+fun CustomUI1Preview() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var value by remember { mutableIntStateOf(0) }
+
+        CircularProgressIndicator(
+            progress = { value / 1000f },
+            modifier = Modifier
+                .size(200.dp),
+            color = ProgressIndicatorDefaults.circularColor,
+            strokeWidth = ProgressIndicatorDefaults.CircularStrokeWidth,
+            trackColor = ProgressIndicatorDefaults.circularIndeterminateTrackColor,
+            strokeCap = ProgressIndicatorDefaults.CircularDeterminateStrokeCap,
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            value = value.toString(),
+            onValueChange = {
+                value = if (it.isNotEmpty()) {
+                    it.toInt()
+                } else {
+                    0
+                }
+            }
+        )
+
+    }
+}
+
+
--- app/src/main/java/com/example/newstart/MainActivity.kt
+++ app/src/main/java/com/example/newstart/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.newstart.ui.CoilImage
 import com.example.newstart.ui.theme.NewStartTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +12,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewStartTheme {
-
 
 
             }
